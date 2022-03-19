640c292f967f0dd456af8934161bdebbb00444c7
housssem1999 <62448561+housssem1999@users.noreply.github.com>
2022-03-19 22:05:30 +0100

    Changed port to 80

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.get('/', function(req,res){
     res.sendFile(__dirname + '/INNO/INNO.html');
 });
 
-var server = app.listen(8081, function(){
+var server = app.listen(80, function(){
     var port = server.address().port;
-    console.log("Server started at http://localhost:%s", port);
+    var host = server.address().host;
+    console.log("Server started at http://%s:%s", host, port);
 });
